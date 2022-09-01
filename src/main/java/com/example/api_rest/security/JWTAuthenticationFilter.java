645549b8dc54955ab5bf5cb3f6da38edf1a3cb71
FelipeGomezGarcia <feli.goga@gmail.com>
2022-09-01 18:40:21 +0200

    Capa de seguridad

---
+++ src/main/java/com/example/api_rest/security/JWTAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.example.api_rest.security;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.api_rest.dto.Usuario;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		try {
+			Usuario credenciales = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+
+			return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					credenciales.getNombreUsuario(), credenciales.getContrasena(), new ArrayList<>()));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+
+		String token = Jwts.builder().setIssuedAt(new Date()).setIssuer("hotelio")
+				.setSubject(((User)auth.getPrincipal()).getUsername())
+				.setExpiration(new Date(System.currentTimeMillis() + 864_000_000))
+				.signWith(SignatureAlgorithm.HS512, "1234").compact();
+		response.addHeader("Authorization", "Bearer " + " " + token);//devuelve token por cabecera
+		response.getWriter().write("{\"token\": \"" + token + "\"}");//devuelve token por body
+		System.out.println(response.getHeader("Authorization"));
+	
+	}
+	
+	
+	
+}
